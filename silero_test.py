1f9ea00189d77b6b4217e638ff75e777f36fab69
Lavrentiev Vladimir <lavrentiev@lanit.ru>
2024-11-15 13:24:02 +0300

    add test file for silero stt

---
+++ silero_test.py
@@ -0,0 +1,21 @@
+import os
+import torch
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'v4_ru.pt'
+
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
+                                   local_file)
+
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+example_text = 'В недрах тундры выдры в г+етрах т+ырят в вёдра ядра кедров.'
+sample_rate = 48000
+speaker='baya'
+
+audio_paths = model.save_wav(text=example_text,
+                             speaker=speaker,
+                             sample_rate=sample_rate)
